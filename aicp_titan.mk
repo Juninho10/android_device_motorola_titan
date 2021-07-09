7dacace6cde74dc2f03874d9d0ff8c3184aac684
Juninho10 <eliascordeiro835@gmail.com>
2021-07-09 15:11:14 -0300

    Adapting tree to build aicp

--- aicp_titan.mk
+++ aicp_titan.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := lineage_titan
+PRODUCT_NAME := aicp_titan
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
